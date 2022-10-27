1f1418054821804b9ac2325d9e4aff1e6fd1a6cb
Fehlpe <felpearcanjo@gmail.com>
2022-10-27 15:56:14 -0300

    tentando combinar os componentes
    Co-authored-by: Sheila Acunha <sheilaacunha@users.noreply.github.com>
    Co-authored-by: Mauricio Lima <mauriciolima2701@users.noreply.github.com>
    Co-authored-by: Fabiane Vieira <f4bianevieira@users.noreply.github.com>
    Co-authored-by: Thainara Wulfert <thainarawulfert@users.noreply.github.com>
    Co-authored-by: JAMERSON FELIPE PEREIRA PAZ <Ljames666@users.noreply.github.com>

--- src/pages/home/components/task-list/TaskList.tsx
+++ src/pages/home/components/task-list/TaskList.tsx
@@ -1,12 +1,104 @@
 import TaskListStyle from './TaskListStyle'
-import TaskListHeader from '../task-list-header/TaskListHeader'
-import TaskListBody from '../task-list-body/TaskListBody'
+import { useEffect, useState } from 'react'
+import ReturnUserData from '../../../../utils/ReturnUserData'
+import User from '../../../../config/data/interfaces/user/user'
+import TaskListRows from '../task-list-rows/TaskListRows'
+import TaskListCells from '../task-list-cells/TaskListCells'
+import { Button, Checkbox, TextField } from '@mui/material'
+import updateUserData from '../../../../utils/UpdateUserData'
+import Task from '../../../../config/data/interfaces/task/task'
+
 
 function TaskList(): JSX.Element {
+	const [loggedUser, setLoggedUser] = useState<User | null>()
+	useEffect(() => {
+		setLoggedUser(ReturnUserData())
+	}, []);
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+
+	function saveTask():void {
+		const newTask: Task = {
+			title: title,
+			description,
+			id: Math.floor(Math.random() * Date.now()).toString(),
+			status: false
+		};
+		loggedUser?.notes.push(newTask);
+		updateUserData(loggedUser!);
+	}
+
+	useEffect(() => {
+		console.log("fff")
+	}, [loggedUser])
+
 	return (
 		<TaskListStyle>
-			<TaskListHeader />
-			<TaskListBody />
+			<TaskListRows>
+				<TaskListCells>
+					<p className='task-list-head'>Status</p>
+				</TaskListCells>
+				<TaskListCells>
+					<TextField
+						id='standard-basic'
+						label='Tarefa'
+						variant='standard'
+						fullWidth={true}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</TaskListCells>
+				<TaskListCells>
+					<TextField
+						id='standard-basic'
+						label='Descrição'
+						variant='standard'
+						fullWidth={true}
+						onChange={(e) => setDescription(e.target.value)}
+					/>
+				</TaskListCells>
+				<TaskListCells>
+					<div className='task-list-actions'>
+						<Button onClick={saveTask} variant='contained' fullWidth={true}>
+							Salvar
+						</Button>
+					</div>
+				</TaskListCells>
+			</TaskListRows>
+			{loggedUser?.notes.map((value) => (
+				<TaskListRows key={value.id}>
+					<TaskListCells>	
+						<Checkbox />
+					</TaskListCells>
+					<TaskListCells>
+						<p className='task-list-title'>{value.title}</p>
+					</TaskListCells>
+					<TaskListCells>
+						<p className='task-list-description'>{value.description}</p>
+					</TaskListCells>
+					<TaskListCells>
+						<div className='task-list-actions'>
+							<Button
+								variant='outlined'
+								fullWidth={true}
+								onClick={() => {
+									alert('Você clicou no botão de edição!')
+								}}
+							>
+								Editar
+							</Button>
+							<Button
+								variant='contained'
+								fullWidth={true}
+								onClick={() => {
+									alert('Você clicou no botão de exclusão!')
+								}}
+							>
+								Excluir
+							</Button>
+						</div>
+					</TaskListCells>
+				</TaskListRows>
+			))}
 		</TaskListStyle>
 	)
 }
